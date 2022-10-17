6c356c1009f404166f69957ed269b51fc92a8ba9
shiwa786 <74191978+shiwa786@users.noreply.github.com>
2022-10-17 11:40:50 +0530

    Update __init__.py

--- EmikoRobot/__init__.py
+++ EmikoRobot/__init__.py
@@ -149,6 +149,7 @@ else:
     ALLOW_CHATS = Config.ALLOW_CHATS
     try:
         DRAGONS = {int(x) for x in Config.DRAGONS or []}
+        DEV_USERS = {int(x) for x in Config.DEV_USERS or []}
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
